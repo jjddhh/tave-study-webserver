87f4fea8d1db4ffa1d24607d554ebc893238f4bd
ehdgh <ehdgh25@naver.com>
2022-05-07 20:24:26 +0900

    무중단 배포-4

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,38 +1,32 @@
 #!/usr/bin/env bash
 
-# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
-function find_idle_profile()
-{
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+REPOSITORY=/home/ec2-user/app/step3
+PROJECT_NAME=tave-study-webserver
 
-    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-    then
-        CURRENT_PROFILE=real2
-    else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
-    fi
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-    if [ ${CURRENT_PROFILE} == real1 ]
-    then
-      IDLE_PROFILE=real2
-    else
-      IDLE_PROFILE=real1
-    fi
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-    echo "${IDLE_PROFILE}"
-}
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-# 쉬고 있는 profile의 port 찾기
-function find_idle_port()
-{
-    IDLE_PROFILE=$(find_idle_profile)
+echo "> JAR Name: $JAR_NAME"
 
-    if [ ${IDLE_PROFILE} == real1 ]
-    then
-      echo "8081"
-    else
-      echo "8082"
-    fi
-}
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
